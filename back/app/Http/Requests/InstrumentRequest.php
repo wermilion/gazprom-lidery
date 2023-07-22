7ddcd242eca816efd589350e90f7e7fdba1c4a92
Bodya <bog.dan682005@mail.ru>
2023-07-22 14:50:00 +0700

    #3648 fix: removed unused request & removed unuses message of InstrumentRequest

--- back/app/Http/Requests/InstrumentRequest.php
+++ back/app/Http/Requests/InstrumentRequest.php
@@ -23,7 +23,6 @@ class InstrumentRequest extends FormRequest
     {
         return [
             'min_points.required' => 'не пустым.',
-            'min_points.unique' => 'уникальным.',
             'min_points.numeric' => 'числом.',
             'task.string' => 'не пустым.',
             'task.max' => 'не более 200 символов.',

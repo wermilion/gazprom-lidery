9067c61c1cdb078d3010ad1b5a2773c7cb2999a0
Bodya <bodyagame682005@gmail.com>
2023-07-28 14:39:01 +0700

    #3648 refactor: removed dark theme from login page

--- back/resources/views/layouts/guest.blade.php
+++ back/resources/views/layouts/guest.blade.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Лидеры Газпрома</title>
-        <link rel="shortcut icon" href="{{asset('favicon.ico')}}">
+    <title>Лидеры Газпрома</title>
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        {{Vite::useBuildDirectory('cp')}}
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+    {{Vite::useBuildDirectory('cp')}}
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <img draggable="false" src="{{asset('cp/images/gazprom-logo.svg')}}" alt="gazprom">
-            <div class="w-full sm:max-w-md mt-6 mb-20 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans text-gray-900 antialiased">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <img draggable="false" src="{{asset('cp/images/gazprom-logo.svg')}}" alt="gazprom">
+    <div class="w-full sm:max-w-md mt-6 mb-20 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+</div>
+</body>
 </html>
